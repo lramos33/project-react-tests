9978fecbcacd05c0c05c56ab05e240afc3b2bbce
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-11-07 21:16:21 -0300

    commit teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App.js';
+
+
+test('App Component', () => {
+  render(<App />);
+
+  const homeLink = screen.getAllByRole('link', { name: 'Home'});
+  const aboutLink = screen.getAllByRole('link', { name: 'About'});
+  const favoritePokemonsLink = screen.getAllByRole('link', { name: 'Favorite Pokémons'});
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(favoritePokemonsLink).toBeInTheDocument();
+});
