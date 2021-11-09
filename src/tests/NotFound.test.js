72bbc68d64bb161aca5b1268bbf2cd04daae49c8
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-11-08 21:35:31 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testing NotFound Component', () => {
+  test('if there is a heading with certain text', () => {
+    render(<NotFound />);
+    const heading = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('if there is an image with certain src', () => {
+    render(<NotFound />);
+    const notFoundImage = screen.getByRole('img', { name: /Pikachu crying/i });
+    expect(notFoundImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
